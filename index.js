707e8c8458b5b67e34e38eb2aac6cdafa612fcd8
tungchan2004 <tunglay12310@gmail.com>
2024-11-19 14:49:00 +0700

    update index

--- index.js
+++ index.js
@@ -15,12 +15,11 @@ mongoose.connect('mongodb+srv://tunglatoi2004:tunglatoi2004@cluster0.4mxgk.mongo
     console.error('Error connecting to MongoDB:', err);
 });
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Cho phép tất cả nguồn gốc
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Cho phép các phương thức
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Cho phép các tiêu đề
-    next();
-});
+app.use(cors({
+    origin: '*',
+    methods: '*',
+    allowedHeaders: '*',
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
